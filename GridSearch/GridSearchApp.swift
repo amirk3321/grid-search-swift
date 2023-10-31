32956f6d81c35c147333e293436cefde6f985a42
amirk3321 <amirk3321@gmail.com>
2023-10-31 15:10:12 +0500

    Initial Commit

---
+++ GridSearch/GridSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchApp.swift
+//  GridSearch
+//
+//  Created by MA on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
